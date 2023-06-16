664efde2b05abdce64f5e96d7a75fa2d2592f0be
Sive Mdluli <mdlulisive202@gmail.com>
2023-06-16 15:32:59 +0200

    Fix editing functionality after reverting to uncorrupted commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
 
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: './src/index.html',
+      title: "Output Management",
+      template: "./src/index.html",
     }),
   ],
 
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
 
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
 
   resolve: {
     alias: {
-      'node-fetch$': 'node-fetch/lib/index.js',
+      "node-fetch$": "node-fetch/lib/index.js",
     },
   },
 };
